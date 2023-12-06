b1f767df9f2717092f7255204608c245f3e3e370
Brandon Susini <brandonsusini@gmail.com>
2023-12-05 22:28:06 -0500

    Fixed weird sprite morphing but removed all polygons. Need to remake the polygons to finish character

--- robot.gd
+++ robot.gd
@@ -7,8 +7,8 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func _process(delta):
-	left_arm_target.position = get_global_mouse_position()
+#func _process(delta):
+#	left_arm_target.position = get_global_mouse_position()
 	
 	
 func _physics_process(delta):
